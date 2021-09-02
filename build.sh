61dd940d4ae04965b357078ed94e3708f1644bc4
Jett Thompson <jett.thompson.59@gmail.com>
2021-09-02 20:44:14 +1000

    fixed build.sh to work with the new image

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -e
 
 docker build --platform linux/amd64 -t aws-lambda-cpp-mongodb .
-docker create -ti --name build-the-program aws-lambda-cpp /bin/bash
+docker create -ti --name build-the-program aws-lambda-cpp-mongodb /bin/bash
 mkdir -p bin
 docker cp build-the-program:/app/main ./bootstrap
 docker rm -f build-the-program
