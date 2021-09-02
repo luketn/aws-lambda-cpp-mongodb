e9200524a91e8a52636722f3253343269470a259
Luke Thompson <endeavour9@gmail.com>
2021-09-02 20:53:48 +1000

    make the http api handle all paths

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@ set -e
 
 docker build --platform linux/amd64 -t aws-lambda-cpp-mongodb .
 docker create -ti --name build-the-program aws-lambda-cpp-mongodb /bin/bash
-mkdir -p bin
 docker cp build-the-program:/app/main ./bootstrap
 docker rm -f build-the-program
+
 rm -f bootstrap.zip
 zip bootstrap bootstrap
